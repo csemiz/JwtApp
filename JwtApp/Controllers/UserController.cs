4247dc638eec27a246bd8071017bd0088f9b7212
Ceren Semiz <ceren_semiz@outlook.com>
2023-10-25 19:17:41 +0300

    Add project files.

---
+++ JwtApp/Controllers/UserController.cs
@@ -0,0 +1,59 @@
+﻿using JwtApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace JwtApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("Admins")]
+        [Authorize(Roles = "Administrator")]
+        public IActionResult SellerEndpoint()
+        {
+            var currentUser = GetCurrentUser();
+
+            return Ok($"Hi {currentUser.GivenName}, you are a {currentUser.Role}");
+        }
+
+        [HttpGet("Sellers")]
+        [Authorize(Roles = "Seller")]
+        public IActionResult AdminsEndpoint()
+        {
+            var currentUser = GetCurrentUser();
+
+            return Ok($"Hi {currentUser.GivenName}, you are an {currentUser.Role}");
+        }
+
+
+        [HttpGet("Public")]
+        public IActionResult Public()
+        {
+            return Ok("Hi, you are on public property");
+        }
+
+
+        private UserModel GetCurrentUser()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+
+            if (identity != null)
+            {
+                var userClaims = identity.Claims;
+
+
+                return new UserModel
+                {
+                    UserName = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.NameIdentifier)?.Value,
+                    EmailAddress = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Email)?.Value,
+                    GivenName = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.GivenName)?.Value,
+                    Surname = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Surname)?.Value,
+                    Role = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Role)?.Value
+                };
+            }
+            return null;
+        }
+    }
+}
---
+++ JwtApp/Models/UserConstants.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+
+namespace JwtApp.Models
+{
+    public class UserConstants
+    {
+
+
+        public static List<UserModel> Users = new List<UserModel>()
+        {
+            new UserModel() { UserName = "jason_admin",EmailAddress = "jason.admin@email.com", Password = "MyPass_word", GivenName = "Jason", Surname = "Bryant", Role = "Administrator" },
+            new UserModel() { UserName = "elyse_seller", EmailAddress = "elyse.seller@email.com", Password = "MyPass_word", GivenName = "Elyse", Surname = "Lambert", Role = "Seller" },
+
+        };
+
+    }
+}
